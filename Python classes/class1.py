9f5c18512859c734362f57bd70da1a691eabbbda
Bhavin-coder <dhanyasajith3@gmail.com>
2025-06-01 19:59:21 +0530

    Add files via upload

---
+++ Python classes/class1.py
@@ -0,0 +1,12 @@
+#activity 1
+#print("my name is bhavin")
+print("Hello")
+print(25678*98763)
+print(677/233)
+
+#activity 2
+print("step1=addition started")
+print("step2=number1=20")
+print("step3=number2=10")
+print("step4=add number1 and number2")
+print("step5=output=30")
